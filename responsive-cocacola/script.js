23af30a844324d74d73208ca8e1b028ca7386943
rgdmprmd <ranggadpermadi@gmail.com>
2021-01-13 14:07:31 +0700

    Staging style

--- responsive-cocacola/script.js
+++ responsive-cocacola/script.js
@@ -1,1 +1,25 @@
-const day = "";
+let swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    loop: true,
+});
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
